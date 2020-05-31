31b86892c26004d9f891497425722c2ed4bb4bf7
Иван Лузиков <iluzikov@gmail.com>
2020-05-31 09:17:13 +0300

    Дабавлен статичесский класс MailSettings для хранения настроек smtp-сервера

---
+++ MailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfMailSender
+{
+    public static class MailSettings
+    {
+        public static string smtpServer = "smtp.mail.ru";
+        public static int smtpServerPort = 25;
+        public static string emailFrom { get; set; }
+        public static string EmailTo { get; set; }
+        public static string Password { get; set; }
+
+    }
+}
