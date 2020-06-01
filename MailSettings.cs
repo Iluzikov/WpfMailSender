da0287f9d2f8041658c444b7ed6f029076df752f
Иван Лузиков <iluzikov@gmail.com>
2020-06-01 22:36:58 +0300

    реализован интерфейс INotifyPropertyChanged в класе MailSettings

--- MailSettings.cs
+++ MailSettings.cs
@@ -1,12 +1,94 @@
-﻿namespace WpfMailSender
-{
-    public static class MailSettings
-    {
-        public static string smtpServer = "smtp.mail.ru";
-        public static int smtpServerPort = 25;
-        public static string emailFrom { get; set; }
-        public static string EmailTo { get; set; }
-        public static string Password { get; set; }
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
+namespace WpfMailSender
+{
+    public class MailSettings : INotifyPropertyChanged
+    {
+        private static string _smtpServer;
+        private static int _smtpServerPort;
+        private string _emailFrom;
+        private string _password;
+        private string _emailTo;
+        private string _emailSubject;
+        private string _emailText;
+
+        public string SmtpServer
+        {
+            get => _smtpServer;
+            set
+            {
+                if (value == _smtpServer) return;
+                _smtpServer = value;
+                OnPropertyChanged(nameof(_smtpServer));
+            }
+        }
+        public int SmtpServerPort
+        {
+            get => _smtpServerPort;
+            set
+            {
+                if (value == _smtpServerPort) return;
+                _smtpServerPort = value;
+                OnPropertyChanged(nameof(_smtpServerPort));
+            }
+        }
+        public string EmailFrom
+        {
+            get => _emailFrom;
+            set
+            {
+                if (value == _emailFrom) return;
+                _emailFrom = value;
+                OnPropertyChanged(nameof(_emailFrom));
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (value == _password) return;
+                _password = value;
+                OnPropertyChanged(nameof(_password));
+            }
+        }
+        public string EmailTo
+        {
+            get => _emailTo;
+            set
+            {
+                if (value == _emailTo) return;
+                _emailTo = value;
+                OnPropertyChanged(nameof(_emailTo));
+            }
+        }
+        public string EmailSubject
+        {
+            get => _emailSubject;
+            set
+            {
+                if (value == _emailSubject) return;
+                _emailSubject = value;
+                OnPropertyChanged(nameof(_emailSubject));
+            }
+        }
+        public string EmailText
+        {
+            get => _emailText;
+            set
+            {
+                if (value == _emailText) return;
+                _emailText = value;
+                OnPropertyChanged(nameof(_emailText));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
     }
 }
