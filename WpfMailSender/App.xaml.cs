248484155f8f358d9bce5f68c227e49b8447752f
Иван Лузиков <iluzikov@gmail.com>
2020-06-10 15:37:10 +0300

    Установка хоста, запуск - остановка хоста

--- WpfMailSender/App.xaml.cs
+++ WpfMailSender/App.xaml.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
+using WpfMailSender.Services;
+using WpfMailSender.ViewModels;
 
 namespace WpfMailSender
 {
@@ -13,5 +12,29 @@ namespace WpfMailSender
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _host;
+        public static IHost Host => _host ?? Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StopAsync().ConfigureAwait(false);
+        }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _host = null;
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataAccessService>();
+            services.AddSingleton<EmailInfoViewModel>();
+        }
+
     }
 }
--- WpfMailSender/Program.cs
+++ WpfMailSender/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +17,21 @@ namespace WpfMailSender
             app.InitializeComponent();
             app.Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(Args);
+            hostBuilder.UseContentRoot(Environment.CurrentDirectory);
+            hostBuilder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            hostBuilder.ConfigureServices(App.ConfigureServices);
+
+            return hostBuilder;
+        }
+
     }
 }
