8f1381e16be0813febf352f1b6f5d0b5c5a2aa08
Иван Лузиков <iluzikov@gmail.com>
2020-06-14 11:38:05 +0300

    Добавлен поиск адресатов

--- WpfMailSender/ViewModels/EmailInfoViewModel.cs
+++ WpfMailSender/ViewModels/EmailInfoViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Data;
 using System.Windows.Input;
 using WpfMailSender.Commands;
 using WpfMailSender.Data;
@@ -14,24 +18,64 @@ namespace WpfMailSender.ViewModels
 
         private readonly IDataAccessService _dataAccessService;
 
-        private string _myFilter;
-        public string MyFilter
+        #region Поиск адресата
+
+        private string _emailsFilterText;
+        public string EmailsFilterText
         {
-            get => _myFilter;
-            set => Set(ref _myFilter, value);
+            get => _emailsFilterText;
+            set 
+            {
+                if(!Set(ref _emailsFilterText, value)) return;
+                _emailsListCollections.View.Refresh();
+            }
         }
+        private readonly CollectionViewSource _emailsListCollections = new CollectionViewSource();
+        public ICollectionView EmailsListCollection => _emailsListCollections?.View;
+
+        private bool _emailsFilterTextFlag = false;
+        public bool EmailsFilterTextFlag 
+        {
+            get => _emailsFilterTextFlag;
+            set => Set(ref _emailsFilterTextFlag, value);
+        }
+        #endregion
 
         private ObservableCollection<Emails> _emailsList;
         public ObservableCollection<Emails> EmailsList
         {
             get => _emailsList;
-            set => Set(ref _emailsList, value);
+            set
+            {
+                if (!Set(ref _emailsList, value)) return;
+                _emailsListCollections.Source = value;
+                OnPropertyChanged(nameof(EmailsListCollection));
+            }
+        }
+
+        private void OnEmailFiltered(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Emails emails))
+            {
+                e.Accepted = false;
+                return; 
+            }
+
+            var filterText = _emailsFilterText;
+            if (string.IsNullOrWhiteSpace(filterText))
+                return;
+
+            if (emails.Name.ToLower().Contains(filterText.ToLower())) return;
+            if (emails.Email.ToLower().Contains(filterText.ToLower())) return;
+
+            e.Accepted = false;
         }
 
         public EmailInfoViewModel(IDataAccessService dataService)
         {
             _dataAccessService = dataService;
             GetEmailsCommand = new RelayCommand(OnGetEmailsCommandExecuted, CanGetEmailsCommandExecute);
+            _emailsListCollections.Filter += OnEmailFiltered;
         }
 
         /// <summary>
@@ -40,6 +84,7 @@ namespace WpfMailSender.ViewModels
         void GetEmails()
         {
             EmailsList = _dataAccessService.GetEmails();
+            EmailsFilterTextFlag = true;
         }
 
         #region Команды получения списка Email
