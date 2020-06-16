8b1aff8c944ad6e50a1a21b08b68bbcd7a358fd6
Иван Лузиков <iluzikov@gmail.com>
2020-06-16 14:37:05 +0300

    Добавлен тест библиотеки шифрования пароля

---
+++ CodePasswordDll.Test/CodePasswordTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodePasswordDLL;
+namespace CodePasswordDll.Test
+{
+    [TestClass]
+    public class CodePasswordTest
+    {
+        [TestMethod]
+        public void GetCodePassword_abc_bcd()
+        {
+            // arrange
+            string strIn = "abc";
+            string strExpected = "bcd";
+
+            // act
+            string strActual = CodePassword.EncryptPassword(strIn);
+
+            // assert
+            Assert.AreEqual(strExpected, strActual);
+        }
+    }
+}
